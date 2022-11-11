03a7d3e98e6cf997c155bb76afd269424479a6ff
Ryan Canton <ryancanton999@gmail.com>
2022-11-11 15:10:55 -0700

    Feat: Define make_keys method to create random integer keys

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,10 @@
 class Enigma
 
-  attr_reader :alphabet
+  attr_reader :alphabet,
+              :key_a,
+              :key_b,
+              :key_c,
+              :key_d
 
   def initialize
     @alphabet = ("a".."z").to_a << " "
@@ -9,7 +13,11 @@ class Enigma
   end
 
   def make_keys
-
+    keys = 5.times.map{rand(10)}.join.to_i.digits
+    @key_a = keys[0]
+    @key_b = keys[1]
+    @key_c = keys[2]
+    @key_d = keys[3]
   end
 
   def make_offsets
