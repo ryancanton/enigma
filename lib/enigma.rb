8d0b7faf1815c59f532d80ee96145253c5c769ab
Ryan Canton <ryancanton999@gmail.com>
2022-11-11 14:43:53 -0700

    Feat: Add empty methods to enigma Class to pass first test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,19 @@
 class Enigma
 
 
+  def initialize
+    @alphabet = ("a".."z").to_a << " "
+    make_keys
+    make_offsets
+  end
+
+  def make_keys
+
+  end
+
+  def make_offsets
+
+  end
 
 
 end
