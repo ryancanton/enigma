e88d2a6379b039341fb2625b98870ad4c1697aff
Ryan Canton <ryancanton999@gmail.com>
2022-11-11 14:56:01 -0700

    Test: Add test for keys a-d for class Enigma

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -14,5 +14,18 @@ RSpec.describe Enigma do
       expect(enigma.alphabet).to be_a(Array)
       expect(enigma.alphabet).to eq(("a".."z").to_a << " ")
     end
+
+    it 'has keys' do
+      enigma = Enigma.new
+
+      expect(enigma.key_a).to be_a(Integer)
+      expect(enigma.key_a.digits.length).to eq(1)
+      expect(enigma.key_b).to be_a(Integer)
+      expect(enigma.key_b.digits.length).to eq(1)
+      expect(enigma.key_c).to be_a(Integer)
+      expect(enigma.key_c.digits.length).to eq(1)
+      expect(enigma.key_d).to be_a(Integer)
+      expect(enigma.key_d.digits.length).to eq(1)
+    end
   end
 end
