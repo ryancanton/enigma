51d25c35c5c4ab6dca0a6f1a9984e96eeb513f32
Ryan Canton <ryancanton999@gmail.com>
2022-11-12 10:21:51 -0700

    Feat: Add make_shifts method for class Enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -14,6 +14,7 @@ class Enigma
     @alphabet = ("a".."z").to_a << " "
     make_keys
     make_offsets
+    make_shifts
   end
 
   def make_keys
@@ -26,7 +27,7 @@ class Enigma
 
   def date_to_offsets
     time = Time.now
-    date_num = [time.day, time.month, time.year].join.to_i**2
+    date_num = [time.day, time.month, time.year.to_s[-2..-1].to_i].join.to_i**2
     offsets = date_num.to_s[-4..-1].to_i.digits
   end
 
@@ -38,5 +39,12 @@ class Enigma
     @offset_d = offsets[3]
   end
 
-
+  def make_shifts
+    @shifts = {
+      A: offset_a + key_a,
+      B: offset_b + key_b,
+      C: offset_c + key_c,
+      D: offset_d + key_d
+    }
+  end
 end
